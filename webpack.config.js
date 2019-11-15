19592b23821c4916c5e285fccdbad4c3e04e841a
tundeiness <tundeiness@yahoo.co.uk>
2019-11-15 16:11:28 +0100

    add webpack css loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use:[{
+          loader: 'style-loader'
+        },{
+          loader: 'css-loader'
+        }]
+      }
     ],
   },
 };
