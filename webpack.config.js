0936f0e5eeb60024722ea07ff5f3c4653fd8e18f
tundeiness <tundeiness@yahoo.co.uk>
2019-11-15 09:12:28 +0100

    add background image

--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: / \.js$ / ,
+        test: / \.js$ /,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
-          options:{
-            presets:['@babel/preset-env']
-          }
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
